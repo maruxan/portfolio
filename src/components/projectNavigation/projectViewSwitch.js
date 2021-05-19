a2364473031ff5b5a13d1f89c0f18cbf48d31c64
maruxan <garavagliamauricio@gmail.com>
2021-05-19 00:22:51 -0300

    buttons content fix

--- src/components/projectNavigation/projectViewSwitch.js
+++ src/components/projectNavigation/projectViewSwitch.js
@@ -7,22 +7,28 @@ export default function ProjectViewSwitch({ currentView, switchView }) {
 
   return (
     <div className="flex text-gray-500 text-xl">
-      <div role="button" tabIndex="0" onKeyPress={keyPressHandler}>
+      <div
+        role="button"
+        tabIndex="0"
+        onKeyPress={keyPressHandler}
         onClick={() => switchView('gallery')}
-        className=
-        {`hover:text-gray-900 ${
+        className={`hover:text-gray-900 ${
           currentView === 'gallery' ? 'text-gray-900' : ''
-        } `}
-        > gallery
+        } `}>
+        {' '}
+        gallery
       </div>
       <div className="mx-2">/</div>
-      <div role="button" tabIndex="0" onKeyPress={keyPressHandler}>
+      <div
+        role="button"
+        tabIndex="0"
+        onKeyPress={keyPressHandler}
         onClick={() => switchView('index')}
-        className=
-        {`hover:text-gray-900 ${
+        className={`hover:text-gray-900 ${
           currentView === 'index' ? 'text-gray-900' : ''
-        } `}
-        > index
+        } `}>
+        {' '}
+        index
       </div>
     </div>
   );
