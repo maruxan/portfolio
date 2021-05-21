44979f9666532d99dac200d7853336b6a63311c5
maruxan <garavagliamauricio@gmail.com>
2021-05-20 21:32:21 -0300

    mobile mockup on project index view for large viewports

--- src/components/projectIndex/projectIndex.js
+++ src/components/projectIndex/projectIndex.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import { a, useSpring } from 'react-spring';
 
 import ProjectIndexTable from './projectIndexTable';
 
 export default function ProjectIndex({ projects }) {
+  const [currentProject, setCurrentProject] = useState();
+  const [showMockup, setShowMockup] = useState(false);
+
+  // Shows the mockup of the current hovered project on the table
+  const projectHoverHandler = (eventType, projectIndex) => {
+    setCurrentProject(projectIndex);
+
+    if (eventType === 'enter') setShowMockup(true);
+    else if (eventType === 'leave') setShowMockup(false);
+  };
+
+  const spring = useSpring({
+    to: { opacity: showMockup ? 1 : 0, y: showMockup ? 0 : 20 },
+    from: { opacity: 0, y: 20 },
+  });
+
   return (
-    <div className="min-h-screen w-full">
-      <div className="w-11/12 mx-auto my-20 pr-2">
-        <ProjectIndexTable projects={projects} />
+    <div className="h-full w-full">
+      <div className="w-11/12 mx-auto my-20 pr-2 flex">
+        <a.div className="w-1/4 h-full sticky hidden lg:block" style={spring}>
+          <StaticImage
+            src="../../images/marq-mobile.png"
+            alt="Project mobile view"
+            className={`w-full mx-auto block absolute top-6 right-6`}
+            onDragStart={(e) => e.preventDefault()}
+          />
+        </a.div>
+        <ProjectIndexTable projects={projects} onHover={projectHoverHandler} />
       </div>
     </div>
   );
--- src/components/projectIndex/projectIndexTable.js
+++ src/components/projectIndex/projectIndexTable.js
@@ -4,9 +4,9 @@ import { Link } from 'gatsby';
 import Technologies from '../ui/technologies';
 import { BsArrowReturnRight } from 'react-icons/bs';
 
-export default function ProjectIndexTable({ projects }) {
+export default function ProjectIndexTable({ projects, onHover }) {
   return (
-    <div className="table-fixed">
+    <div className="table-fixed w-full">
       <thead>
         <tr>
           <th className="border-b border-gray-900 w-3/4">name</th>
@@ -15,8 +15,12 @@ export default function ProjectIndexTable({ projects }) {
         </tr>
       </thead>
       <tbody>
-        {projects.map((project) => (
-          <tr className="transition transform hover:shadow-xl hover:scale-[1.02] rounded-xl group">
+        {projects.map((project, index) => (
+          <tr
+            className="transition transform hover:shadow-xl hover:scale-[1.02] rounded-xl group"
+            onMouseEnter={() => onHover('enter', index)}
+            onMouseLeave={() => onHover('leave', index)}
+            key={index}>
             <td className="transition group-hover:border-transparent md:text-2xl py-4">
               {project.name}
             </td>
