62d9e89d3098a695ec119dba224fb57ef16b711d
maruxan <garavagliamauricio@gmail.com>
2021-08-15 16:19:57 -0300

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#eaeaea`,
         theme_color: `#eaeaea`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-postcss',
@@ -34,4 +34,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
